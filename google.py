bd63d9651ccce08c75ea16a492d77d8a79262a06
101511418+OverWindow@users.noreply.github.com <101511418+OverWindow@users.noreply.github.com>
2022-11-12 17:16:44 +0900

    11.12

--- google.py
+++ google.py
@@ -6,8 +6,8 @@ import urllib.request
 import os
 
 #### 개발자 툴 ####
-searchItem = "dog"
-howMany = 50
+searchItem = "kvaratskhelia"
+howMany = 30
 # 실행방법: python google.py
 # 실행 중 뜨는 오류들은 무시하시면 됩니다
 ###################
